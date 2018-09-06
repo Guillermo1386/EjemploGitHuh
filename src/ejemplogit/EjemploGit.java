e97e9743dc7455e02cd23fbedf27f58c2277356c
USUARIO <USUARIO@pc4-sala210>
2018-09-05 21:25:18 -0500

    - Se borro comentarios.

--- src/ejemplogit/EjemploGit.java
+++ src/ejemplogit/EjemploGit.java
@@ -5,17 +5,12 @@
  */
 package ejemplogit;
 
-/**
- *
- * @author USUARIO
- */
+
 public class EjemploGit {
 
-    /**
-     * @param args the command line arguments
-     */
+   
     public static void main(String[] args) {
-        // TODO code application logic here
+        
     }
     
 }
